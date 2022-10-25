364c415d01b0cce6e343bff69885b176ca963e14
kannangr21 <kannanravi2105@gmail.com>
2022-10-25 19:48:03 +0530

    Added Resources page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const Teams = lazy(() => import("./Pages/Team/Team"));
 const Home = lazy(() => import("./Pages/Home/Home"));
 //const EventMain = lazy(() => import("./Pages/Events"));
 const About = lazy(() => import("./Pages/About/About"));
-//const Resources = lazy(() => import("./Pages/Resources/Resources"));
+const Resources = lazy(() => import("./Pages/Resources/Resources"));
 
 const Loader = () => {
   return (
@@ -29,7 +29,7 @@ const App = () => {
           {/* <Route exact path="/events" component={EventMain} /> */}
           <Route exact path="/teams" component={Teams} />
           <Route exact path="/about" component={About} />
-          {/* <Route exact path="/resources" component={Resources} /> */}
+          <Route exact path="/resources" component={Resources} />
         </Switch>
       </Suspense>
       <Footer />
