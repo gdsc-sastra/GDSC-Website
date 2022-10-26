3f2b0c52db4af08ea4a5dc5c519c92655d4ba778
kannangr21 <kannanravi2105@gmail.com>
2022-10-26 23:38:57 +0530

    GSheet API bug fix

--- src/Pages/Home/ContactUs.js
+++ src/Pages/Home/ContactUs.js
@@ -5,8 +5,8 @@ import { GoogleSpreadsheet } from "google-spreadsheet";
 const SPREADSHEET_ID = process.env.REACT_APP_SPREADSHEET_ID;
 const SHEET_ID = process.env.REACT_APP_SHEET_ID;
 const CLIENT_EMAIL = process.env.REACT_APP_GOOGLE_CLIENT_EMAIL;
-// const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY.replace(/\\n/g, '\n');
-const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY;
+const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY.replace(/\\n/g, '\n');
+// const PRIVATE_KEY = process.env.REACT_APP_GOOGLE_SERVICE_PRIVATE_KEY;
 
 
 const doc = new GoogleSpreadsheet(SPREADSHEET_ID);
